40e6354e87392cb8c7132a729ca37c3f2be94e5d
patricejiang <jiangzhan@chukong-inc.com>
2019-10-11 16:26:01 +0800

    remove comments

--- js/src/build-ios/pt_build_ios.sh
+++ js/src/build-ios/pt_build_ios.sh
@@ -10,33 +10,30 @@ IPHONE_SDK=iphoneos
 
 #MAC_SDK=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
 
-
-PT_TARGET=aarch64-apple-ios
-# #PT_TARGET=aarch64-ios
-# PT_ARCH=aarch64
-# #//PT_ARCH=arm64-applie-darwin
-
-#//PT_TARGET=arm64-apple-ios
-
-# PT_TARGET=armv7s-apple-ios
-# #PT_TARGET=armv7s-ios
-# PT_ARCH=armv7s
-
-PT_TARGET=armv7-apple-ios
-#//PT_TARGET=armv7s-ios
-PT_ARCH=armv7
-
-#PT_FLAGS+="-arch $PT_ARCH"
+arg_arch=$1
 
 
-#PT_CC=`xcrun -sdk iphoneos -find clang` 
-#PT_CXX=`xcrun -sdk iphoneos -find clang++` 
+if [ $arg_arch == "aarch64" ] ; then
+    PT_TARGET=aarch64-apple-ios
+    PT_ARCH=aarch64
+elif [ $arg_arch == "armv7s" ] ; then
+    PT_TARGET=armv7s-apple-ios
+    PT_ARCH=armv7s
+elif [ $arg_arch == "armv7" ] ; then
+    PT_TARGET=armv7-apple-ios
+    PT_ARCH=armv7
+else 
+    echo "no arch or bad arch"
+    exit 1
+fi
 
-#PT_CC="xcrun -sdk iphoneos clang"
-#PT_CXX="xcrun -sdk iphoneos clang++"
 
 PT_CC=/Users/cocos/Downloads/firefox-69.0/js/src/build-ios/cc_link
 PT_CXX=/Users/cocos/Downloads/firefox-69.0/js/src/build-ios/cxx_link
+
+
+#PT_DISABLE_JIT="--disable-jm --disable-methodjit --disable-monoic --disable-polyic"
+PT_DISABLE_JIT=""
 
 
 CFLAGS=" $PT_FLAGS " \
@@ -44,30 +41,19 @@ CXXFLAGS=" $PT_FLAGS " \
 CC=$PT_CC \
 CXX=$PT_CXX \
 ../configure \
-    --with-system-zlib=/Users/cocos/Downloads/zlib-1.2.11/dist \
     --enable-ios-target \
     --without-tests \
     --disable-shared-js \
     --target=$PT_TARGET \
     --disable-js-shell \
     --disable-ion \
+    $PT_DISABLE_JIT \
     --with-ios-sdk=$IPHONE_SDK \
+    --enable-debug
+
+
+
+    #--with-system-zlib=/Users/cocos/Downloads/zlib-1.2.11/dist \
  #   --enable-simulator=arm64
-
-
 #--with-ios-target=iPhoneOS \
-    
-    
 
-# CFLAGS=" $PT_FLAGS " \
-# CXXFLAGS=" $PT_FLAGS " \
-# CC=
-# ../configure \
-#     --enable-ios-target \
-#     --without-tests \
-#     --disable-shared-js \
-#     --target=$PT_TARGET \
-#     --disable-js-shell \
-#     --with-ios-sdk=$IPHONE_SDK \
-#     --with-system-zlib
-#    # --with-macos-sdk=$MAC_SDK
